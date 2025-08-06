823099dc92cae29db3c5de949d2a6902b7a0bea1
Elango-17 <elangovan.ravi3355@gmail.com>
2025-08-06 15:43:01 +0530

    Commiting Project structure for My-Shuttle

---
+++ PageScenarios/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PageScenarios
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
